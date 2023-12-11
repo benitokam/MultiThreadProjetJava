1f52b40eaa8809525204a2e5096193f6cedea0a4
Mamadou cire Camara <mamadoucirecamara@gmail.com>
2023-12-12 00:02:42 +0100

    Methode Affichage

--- ProjetMultiThread/src/main/java/org/example/Main.java
+++ ProjetMultiThread/src/main/java/org/example/Main.java
@@ -10,16 +10,13 @@ import java.util.Collection;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
+
 
 
         long startTime = System.currentTimeMillis();
-        LierUnFichier fichier = new LierUnFichier("/Users/mamadoucirecamara/Desktop/TP_Master/ProjetMultiThread/src/main/resources/PythonNotesForProfessionals.pdf");
-        fichier.search("The difference between .keys() and .iterkeys(), .values() and .itervalues(), .items() and .iteritems() is\n" +
-                "that the iter* methods are generators. Thus, the elements within the dictionary are yielded one by one as they are\n" +
-                "evaluated. When a list object is returned, all of the elements are packed into a list and then returned for further\n" +
-                "evaluation.");
+        LierUnFichier fichier = new LierUnFichier("/Users/mamadoucirecamara/Desktop/TP_Master/MultiThreadProjetJava/ProjetMultiThread/src/main/resources/PythonNotesForProfessionals.pdf");
+        fichier.search("Iterating");
+          fichier.Affichage();
         long endTime = System.currentTimeMillis();
         long elapsedTime = endTime - startTime;
 
